8a0a32fae7fc0157159d8d1241246003bfc80faa
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-31 16:19:15 +0900

    悩み保存機能実装

--- app/controllers/troubles_controller.rb
+++ app/controllers/troubles_controller.rb
@@ -2,6 +2,22 @@ class TroublesController < ApplicationController
 
     def index
         @troubles = Trouble.all.order("id DESC")
+        @trouble = Trouble.new
+    end
+
+    def create
+        @trouble = Trouble.new(trouble_params)
+        if @trouble.save
+            redirect_to troubles_path
+        else
+            render :index
+        end
+    end
+
+    private
+
+    def trouble_params
+        params.require(:trouble).permit(:text)
     end
     
 end
